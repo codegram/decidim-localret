80a46fbfd4cc726f66a17c9cfddd83fb77519adf
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-04-25 11:17:23 +0200

    Add maps

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -20,4 +20,12 @@ Decidim.configure do |config|
 
   # The number of reports which an object can receive before hiding it
   # config.max_reports_before_hiding = 3
+
+  if Rails.application.secrets.geocoder
+    config.geocoder = {
+      static_map_url: "https://image.maps.cit.api.here.com/mia/1.6/mapview",
+      here_app_id: Rails.application.secrets.geocoder["here_app_id"],
+      here_app_code: Rails.application.secrets.geocoder["here_app_code"]
+    }
+  end
 end
