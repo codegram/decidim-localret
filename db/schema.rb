25b162ea5504cd6660350318be7f69ff0660c2f4
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-12-13 15:00:29 +0100

    Fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -191,23 +191,6 @@ ActiveRecord::Schema.define(version: 20171213133533) do
     t.index ["decidim_root_commentable_type", "decidim_root_commentable_id"], name: "decidim_comments_comment_root_commentable"
   end
 
-  create_table "decidim_dummy_resources", force: :cascade do |t|
-    t.string "title"
-    t.text "address"
-    t.float "latitude"
-    t.float "longitude"
-    t.bigint "decidim_feature_id"
-    t.bigint "decidim_author_id"
-    t.bigint "decidim_category_id"
-    t.bigint "decidim_scope_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["decidim_author_id"], name: "index_decidim_dummy_resources_on_decidim_author_id"
-    t.index ["decidim_category_id"], name: "index_decidim_dummy_resources_on_decidim_category_id"
-    t.index ["decidim_feature_id"], name: "index_decidim_dummy_resources_on_decidim_feature_id"
-    t.index ["decidim_scope_id"], name: "index_decidim_dummy_resources_on_decidim_scope_id"
-  end
-
   create_table "decidim_features", id: :serial, force: :cascade do |t|
     t.string "manifest_name"
     t.jsonb "name"
@@ -393,6 +376,8 @@ ActiveRecord::Schema.define(version: 20171213133533) do
     t.string "secondary_hosts", default: [], array: true
     t.string "available_authorizations", default: [], array: true
     t.text "header_snippets"
+    t.jsonb "cta_button_text"
+    t.string "cta_button_path"
     t.index ["host"], name: "index_decidim_organizations_on_host", unique: true
     t.index ["name"], name: "index_decidim_organizations_on_name", unique: true
   end
@@ -530,18 +515,6 @@ ActiveRecord::Schema.define(version: 20171213133533) do
     t.index ["from_type", "from_id"], name: "index_decidim_resource_links_on_from_type_and_from_id"
     t.index ["name"], name: "index_decidim_resource_links_on_name"
     t.index ["to_type", "to_id"], name: "index_decidim_resource_links_on_to_type_and_to_id"
-  end
-
-  create_table "decidim_results_results", id: :serial, force: :cascade do |t|
-    t.jsonb "title"
-    t.jsonb "description"
-    t.integer "decidim_feature_id"
-    t.integer "decidim_scope_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "reference"
-    t.index ["decidim_feature_id"], name: "index_decidim_results_results_on_decidim_feature_id"
-    t.index ["decidim_scope_id"], name: "index_decidim_results_results_on_decidim_scope_id"
   end
 
   create_table "decidim_scope_types", id: :serial, force: :cascade do |t|
